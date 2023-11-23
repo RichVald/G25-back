1801e57df3f2bfb2b34e69b71e2ecb1fbaf37539
Richi <richi.valdivieso@gmail.com>
2023-11-22 21:21:30 -0600

    Rutas con controladores

---
+++ backend/routes/tareasRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const { getTareas, setTareas, updateTareas, deleteTareas } = require('../controllers/tareasController')
+
+router.get('/', getTareas)
+
+router.post('/', setTareas)
+
+router.put('/:id', updateTareas)
+
+router.delete('/:id', deleteTareas)
+
+module.exports = router
